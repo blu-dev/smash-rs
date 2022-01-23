4bade3d4cd79c667be2b393bf4f26f3901a59c0c
blu-dev <the.blu.dev@gmail.com>
2022-01-23 16:43:04 -0700

    move extra ai namespaces to own module

--- src/app.rs
+++ src/app.rs
@@ -5,22 +5,11 @@ mod consts;
 
 
 pub mod ai;
-pub mod ai_camera;
-pub mod ai_dangerzone;
-pub mod ai_debug;
-pub mod ai_deprecated;
-pub mod ai_koopag;
-pub mod ai_notify_event;
-pub mod ai_param;
-pub mod ai_random;
-pub mod ai_rule;
-pub mod ai_stage;
-pub mod ai_system;
-pub mod ai_utility;
-pub mod ai_weapon;
+mod ai_extras;
 
 mod events;
 
+pub use ai_extras::*;
 pub use consts::*;
 pub use events::*;
 
