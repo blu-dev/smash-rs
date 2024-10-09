f23573b8625e910259e2f32d618f73f8120954ea
WuBoytH <alexwuief@gmail.com>
2024-10-09 17:10:48 -0400

    simd pain (thanks latest rust)

--- src/cpp/simd.rs
+++ src/cpp/simd.rs
@@ -1,20 +1,14 @@
 #[repr(simd)]
 pub struct Vector2 {
-    pub x: f32,
-    pub y: f32,
+    pub vec: [f32; 2]
 }
 
 #[repr(simd)]
 pub struct Vector3 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+    pub vec: [f32; 3]
 }
 
 #[repr(simd)]
 pub struct Vector4 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
-    pub w: f32,
+    pub vec: [f32; 4]
 }
